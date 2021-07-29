34c4cdfa0fe0dcb9a1eec5d982582eef87cb7994
Janne Oksanen <janneooksanen@eaton.com>
2021-07-29 11:58:18 +0200

    Changed command char values to new C&C API spec

--- MQTTSNGateway/src/linux/xbee/SensorNetwork.h
+++ MQTTSNGateway/src/linux/xbee/SensorNetwork.h
@@ -33,15 +33,17 @@ namespace MQTTSNGW
   #define D_NWSTACK(...)
 #endif
 
-#define RFM_TX_CMD		         0x10
-#define RFM_RX_CMD	             0x90
-#define API_MODEMSTATUS          0x8A
-#define RFM_RX_ACK               0x8B
+// Data interface
+#define RFM_TX_CMD		         0x02
+#define RFM_RX_ACK               0x03
+#define RFM_RX_CMD	             0x04
+
+// Reserved for serial frame handling
+#define START_BYTE               0x7e
+#define ESCAPE                   0x7d
 
 #define XMIT_STATUS_TIME_OVER    5000
 
-#define START_BYTE               0x7e
-#define ESCAPE                   0x7d
 
 /*===========================================
   Class  SerialPort
